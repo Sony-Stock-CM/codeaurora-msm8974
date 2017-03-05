SpaceKiller/android_kernel_motorola_msm8226

drivers: platform: msm: qpnp-vibrator: Add sysfs interface for intensity
To control the Haptic intensity using sysfs interface.

Signed-off-by: SpaceKiller <alexisalbo.1@gmail.com>

drivers/platform/msm/qpnp-vibrator.c
@@ -10,6 +10,7 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/device.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -57,6 +58,49 @@ struct qpnp_vib {
 };
 
 static struct qpnp_vib *vib_dev;
+
+static ssize_t qpnp_vib_level_show(struct device *dev,
+                                        struct device_attribute *attr,
+                                        char *buf)
+{
+        struct timed_output_dev *tdev = dev_get_drvdata(dev);
+        struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+                                         timed_dev);
+
+        return scnprintf(buf, PAGE_SIZE, "%d\n", vib->vtg_level_haptic);
+}
+
+
+static ssize_t qpnp_vib_level_store(struct device *dev,
+                                        struct device_attribute *attr,
+                                        const char *buf, size_t count)
+{
+        struct timed_output_dev *tdev = dev_get_drvdata(dev);
+        struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+                                         timed_dev);
+        int val;
+        int rc;
+
+        rc = kstrtoint(buf, 10, &val);
+        if (rc) {
+                pr_err("%s: error getting level\n", __func__);
+                return -EINVAL;
+        }
+
+        if (val < QPNP_VIB_MIN_LEVEL) {
+                pr_err("%s: level %d not in range (%d - %d), using min.", __func__, val, QPNP_VIB_MIN_LEVEL, QPNP_VIB_MAX_LEVEL);
+                val = QPNP_VIB_MIN_LEVEL;
+        } else if (val > QPNP_VIB_MAX_LEVEL) {
+                pr_err("%s: level %d not in range (%d - %d), using max.", __func__, val, QPNP_VIB_MIN_LEVEL, QPNP_VIB_MAX_LEVEL);
+                val = QPNP_VIB_MAX_LEVEL;
+        }
+
+        vib->vtg_level_haptic = val;
+
+        return strnlen(buf, count);
+}
+
+static DEVICE_ATTR(vtg_level, S_IRUGO | S_IWUSR, qpnp_vib_level_show, qpnp_vib_level_store);
 
 static int qpnp_vib_read_u8(struct qpnp_vib *vib, u8 *data, u16 reg)
 {
@@ -320,6 +364,8 @@ static int __devinit qpnp_vibrator_probe(struct spmi_device *spmi)
 	rc = timed_output_dev_register(&vib->timed_dev);
 	if (rc < 0)
 		return rc;
+
+        device_create_file(vib->timed_dev.dev, &dev_attr_vtg_level);
 
 	vib_dev = vib;
 

Sony-Stock-CM/codeaurora-msm8974

Change-Id: I04d625ec4b8172b0e534702f6381e4ecc6c94c34

drivers/platform/msm/qpnp-vibrator.c
@@ -10,6 +10,7 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/device.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -50,6 +51,48 @@ struct qpnp_vib {
 };
 
 static struct qpnp_vib *vib_dev;
+
+static ssize_t qpnp_vib_level_show(struct device *dev,
+					struct device_attribute *attr,
+					char *buf)
+{
+	struct timed_output_dev *tdev = dev_get_drvdata(dev);
+	struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+					timed_dev);
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", vib->vtg_level);
+}
+
+static ssize_t qpnp_vib_level_store(struct device *dev,
+					struct device_attribute *attr,
+					const char *buf, size_t count)
+{
+	struct timed_output_dev *tdev = dev_get_drvdata(dev);
+	struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+					timed_dev);
+	int val;
+	int rc;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc) {
+		pr_err("%s: error getting level\n", __func__);
+		return -EINVAL;
+	}
+
+	if (val < QPNP_VIB_MIN_LEVEL) {
+		pr_err("%s: level %d not in range (%d - %d), using min.", __func__, val, QPNP_VIB_MIN_LEVEL, QPNP_VIB_MAX_LEVEL);
+		val = QPNP_VIB_MIN_LEVEL;
+	} else if (val > QPNP_VIB_MAX_LEVEL) {
+		pr_err("%s: level %d not in range (%d - %d), using max.", __func__, val, QPNP_VIB_MIN_LEVEL, QPNP_VIB_MAX_LEVEL);
+		val = QPNP_VIB_MAX_LEVEL;
+	}
+
+	vib->vtg_level = val;
+
+	return strnlen(buf, count);
+}
+
+static DEVICE_ATTR(vtg_level, S_IRUGO | S_IWUSR, qpnp_vib_level_show, qpnp_vib_level_store);
 
 static int qpnp_vib_read_u8(struct qpnp_vib *vib, u8 *data, u16 reg)
 {
@@ -293,6 +336,8 @@ static int __devinit qpnp_vibrator_probe(struct spmi_device *spmi)
 	rc = timed_output_dev_register(&vib->timed_dev);
 	if (rc < 0)
 		return rc;
+
+	device_create_file(vib->timed_dev.dev, &dev_attr_vtg_level);
 
 	vib_dev = vib;
 